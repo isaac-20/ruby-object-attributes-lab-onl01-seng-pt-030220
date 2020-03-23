9cbc28121767f18dbf8a7094a6b86f62af63561d
Isaac Smith <isaacsmith215@aol.com>
2020-03-23 11:59:21 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  
+  def name=(p_name)
+    @name = p_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(p_jobs)
+    @job = p_jobs
+  end
+  
+  def job
+    @job
+  end
+end
